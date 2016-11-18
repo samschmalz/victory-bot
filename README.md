d24a1280baf933823245ec4ec6f8f8f5e4533e4d
samschmalz <samschmalz@github.com>
2016-11-18 08:27:46 -0600

    added existing file, updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # victory-bot
 A bot user for the Discord Chat application
+
+This is a single-file script for now, but it may be expanded in the future if I look into the Discord API a bit more.  For now, it offers some basic functionality in how it operates.
+
+Current features:
+Replies to messages starting with correct commands
+
+To implement:
+* given a group of users, will randomly assign a different user to each user (useful for secret santa or other events).
+* message a list of commands when a help command is issued
+* send a message to another user when the !highfive @user command is issued
---
+++ victoryBot.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3.5
+
+import discord
+import random
+
+client = discord.Client()
+
+victoryID = 0 
+serv = 0
+
+@client.event
+async def on_ready():
+    channelList = client.get_all_channels()
+    for c in channelList:
+        if c.name == 'victories':
+            global victoryID
+            victoryID = str(c.id)
+    for s in client.servers:
+        if s.name == "birbChat":
+            global serv
+            serv = s
+    print("Ready!")
+
+@client.event
+async def on_message(message):
+    tag = '!'
+    if not message.content.startswith(tag):
+        return
+    if message.content.startswith(tag + 'ping'):
+        msg = await client.send_message(message.channel, "pong!")
+    if message.content.startswith(tag + "hello"):
+        msg = await client.send_message(message.channel, "Hello, " + message.author.mention)
+    if message.content.startswith(tag + "victory:"):
+        victory = client.get_channel(victoryID)
+        mess = message.content.split(':')
+        if victory == None:
+            msg = await client.send_message(message.channel, "I can't access victories right now.  Try again later.")
+        else:
+            msg = await client.send_message(victory, message.author.mention + ", " + mess[1])
+    if message.content.startswith(tag + "<3"):
+        rand = random.randint(0,3)
+        if rand == 0:
+            msg = await client.send_message(message.channel, "Aww")
+        elif rand == 1:
+            msg = await client.send_message(message.channel, "I love you too, " + message.author.mention)
+        elif rand == 2:
+            msg = await client.send_message(message.channel, "! == not, <3 == love... do you not love me, " + message.author.mention + "?")
+        elif rand == 3:
+            lauren = serv.get_member_named("AIRHORN SOLUTIONS#6723")
+            msg = await client.send_message(message.channel, "My love belongs to " + lauren.mention)
+        else:
+            return
+

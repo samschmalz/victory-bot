3acc19c7f1a431c0e87063db14acec30729bfc74
samschmalz <sam.schmalzried@gmail.com>
2017-08-13 12:14:57 -0500

    simplified createDB.py

--- createDB.py
+++ createDB.py
@@ -6,11 +6,15 @@ connection = sqlite3.connect(database_file)
 curse = connection.cursor()
 
 #setting up variables to create the table containing "players" scores
-scores_table = "scores"
-scores_player = "player"
-string_type = "TEXT"
-scores_score = "score"
-int_type = "INTEGER"
+#scores_table = "scores"
+#scores_player = "player"
+#string_type = "TEXT"
+#scores_score = "score"
+#int_type = "INTEGER"
+#
+#params = scores_table, scores_player, string_type, scores_score, int_type
+#
+##create table to contain the scores
+#curse.execute("CREATE TABLE ? (? ?, ? ?)", params)
 
-#create table to contain the scores
-curse.execute("CREATE TABLE {p0} ('{p1}' {p2}, '{p3}' {p4})".format(p0=scores_table, p1=scores_player, p2=string_type, p3=scores_score, p4=int_type))
+curse.execute("CREATE TABLE scores (player TEXT, score INTEGER)")
